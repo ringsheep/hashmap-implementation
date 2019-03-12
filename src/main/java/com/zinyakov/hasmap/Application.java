50047e9c48dd09aa3211d4c326685530739953e5
Георгий Зиняков <gzinyakov@alfabank.ru>
2019-03-13 01:55:47 +0300

    Inital commit

---
+++ src/main/java/com/zinyakov/hasmap/Application.java
@@ -0,0 +1,8 @@
+package com.zinyakov.hasmap;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zinyakov/hasmap/models/Bucket.java
@@ -0,0 +1,7 @@
+package com.zinyakov.hasmap.models;
+
+import java.util.LinkedList;
+
+public class Bucket {
+    public LinkedList<Entry> linkedList;
+}
